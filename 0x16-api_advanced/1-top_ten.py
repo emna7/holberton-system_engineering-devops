6b26a96f5810b2ccfa85db219aae2bf4a2737fc4
Emna <bhmemna7@gmail.com>
2020-05-07 17:19:21 +0000

    create

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""contains 1 function: top_ten"""
+
+import requests
+
+
+def top_ten(subreddit):
+    user = {"User-Agent": "custom"}
+    request = requests.get("https://www.reddit.com/r/{}/hot.json"
+                       .format(subreddit), headers=user)
+    try:
+        for i in range(10):
+            print(request.json()
+              .get("data").get("children")[i].get("data").get("title"))
+    except:
+        print("None")
