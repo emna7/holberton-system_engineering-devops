3b4e067bf5cf3ad59fe1cf825f83edad37c61ac7
Emna <bhmemna7@gmail.com>
2020-05-07 21:15:45 +0000

    add

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-"""
+"""a recursive function that queries the Reddit API and returns a list containing the titles of all hot articles for a given subreddit
 """
 
 import requests
 
 
 def recurse(subreddit, hot_list=[]):
-
+    request = requests.get("https://www.reddit.com/r/{}/hot.json"
+                           .format(subreddit), headers=user)
