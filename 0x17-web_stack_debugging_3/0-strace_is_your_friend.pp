355b4e4e09e0052085f4ca56eee11bc0c5d0bdd4
Emna <bhmemna7@gmail.com>
2020-05-20 20:55:01 +0000

    create

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,2 @@
-# Fixing the issue
-exec { 'right path':
-    cwd     => '/var/www/html/',
-    command => '/bin/sed -i -e "s/.phpp/.php/g"  wp-settings.php',
-}
+#fixes a typo in the config
+exec { '/usr/bin/env sed -i "s/phpp/php/g" /var/www/html/wp-settings.php': }
