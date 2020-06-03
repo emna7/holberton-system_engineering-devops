ca2e5202ea22328b3bc581d0d29fd213bc1e0998
Emna <bhmemna7@gmail.com>
2020-06-03 16:45:37 +0000

    pep8

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -2,27 +2,27 @@
 import requests
 import json
 import sys
-users='https://jsonplaceholder.typicode.com/users'
-usr=requests.get(users)
+users = 'https://jsonplaceholder.typicode.com/users'
+usr = requests.get(users)
 for i in usr.json():
-    for key,value in i.items():
-        if key=='id' and value==int(sys.argv[1]):
-            name=i['name']
+    for key, value in i.items():
+        if key == 'id' and value == int(sys.argv[1]):
+            name = i['name']
             print('Employee {} is done with tasks'.format(name), end="")
-j=0
-k=0
-todos='https://jsonplaceholder.typicode.com/todos'
-todo=requests.get(todos)
+j = 0
+k = 0
+todos = 'https://jsonplaceholder.typicode.com/todos'
+todo = requests.get(todos)
 for i in todo.json():
-    for key,value in i.items():
-        if key=='userId' and value==int(sys.argv[1]):
+    for key, value in i.items():
+        if key == 'userId' and value == int(sys.argv[1]):
             if i['title'] is not None:
-                k+=1
-            if i['completed']==True:
-                j+=1
-print ('({}/{}):'.format(j,k))
+                k += 1
+            if i['completed'] is True:
+                j += 1
+print('({}/{}):'.format(j, k))
 for i in todo.json():
-    for key,value in i.items():
-        if key=='userId' and value==int(sys.argv[1]):
-            if i['completed']==True:
+    for key, value in i.items():
+        if key == 'userId' and value == int(sys.argv[1]):
+            if i['completed'] is True:
                 print('\t{}'.format(i['title']))
