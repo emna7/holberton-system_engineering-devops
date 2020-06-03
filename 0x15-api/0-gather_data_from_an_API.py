83930284d5e30dfe23389e0fb512c86a2257891b
Emna <bhmemna7@gmail.com>
2020-06-03 16:59:22 +0000

    create

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,30 +1,32 @@
 #!/usr/bin/python3
-"""module
+"""gathering data
 """
 import requests
 import json
 import sys
-users = 'https://jsonplaceholder.typicode.com/users'
-usr = requests.get(users)
-for i in usr.json():
-    for key, value in i.items():
-        if key == 'id' and value == int(sys.argv[1]):
-            name = i['name']
-            print('Employee {} is done with tasks'.format(name), end="")
-j = 0
-k = 0
-todos = 'https://jsonplaceholder.typicode.com/todos'
-todo = requests.get(todos)
-for i in todo.json():
-    for key, value in i.items():
-        if key == 'userId' and value == int(sys.argv[1]):
-            if i['title'] is not None:
-                k += 1
-            if i['completed'] is True:
-                j += 1
-print('({}/{}):'.format(j, k))
-for i in todo.json():
-    for key, value in i.items():
-        if key == 'userId' and value == int(sys.argv[1]):
-            if i['completed'] is True:
-                print('\t{}'.format(i['title']))
+if __name__ == '__main__':
+    if len(sys.argv) == 2 and sys.argv[1].isdigit():
+        users = 'https://jsonplaceholder.typicode.com/users'
+        usr = requests.get(users)
+        for i in usr.json():
+            for key, value in i.items():
+                if key == 'id' and value == int(sys.argv[1]):
+                    name = i['name']
+                print('Employee {} is done with tasks'.format(name), end="")
+        j = 0
+        k = 0
+        todos = 'https://jsonplaceholder.typicode.com/todos'
+        todo = requests.get(todos)
+        for i in todo.json():
+            for key, value in i.items():
+                if key == 'userId' and value == int(sys.argv[1]):
+                    if i['title'] is not None:
+                        k += 1
+                    if i['completed'] is True:
+                        j += 1
+        print('({}/{}):'.format(j, k))
+        for i in todo.json():
+            for key, value in i.items():
+                if key == 'userId' and value == int(sys.argv[1]):
+                    if i['completed'] is True:
+                        print('\t {}'.format(i['title']))
