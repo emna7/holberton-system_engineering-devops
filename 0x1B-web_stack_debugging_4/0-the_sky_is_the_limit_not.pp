dde1908f3f7de3aee71ee1248f1336a59e7479a3
Emna <bhmemna7@gmail.com>
2020-06-17 10:18:49 +0000

    fixing code

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
-exec { '/etc/default/ngix':
-  command => 'sed -i s/15/4096/g /etc/default/ngix; \
-  /etc/init.d/nginx restart',
-  path    => ['/bin/'],
+# fixing the failing requests
+exec {'/etc/default/ngix':
+path    => ['/usr/bin', '/bin'],
+command => "sudo sed -i 's/-n 15/-n 2000/g' /etc/default/nginx; sudo service nginx restart",
 }
