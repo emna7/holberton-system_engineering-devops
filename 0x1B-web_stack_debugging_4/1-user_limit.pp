4502ec3396ab259ae82b9cd2497f8f29f16887bd
emna7 <bhmemna7@gmail.com>
2020-07-05 17:30:27 +0100

    create

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,10 @@
+# Change the OS configuration so that it is possible to login with the
+# holberton user and open a file without any error message
+exec { 'Correct hard':
+  command => 'sudo sed -i \'s/nofile 5/nofile 30000/\' /etc/security/limits.conf',
+  provider => shell,
+}
+exec { 'Correct soft':
+  command  => 'sudo sed -i \'s/nofile 4/nofile 10000/\' /etc/security/limits.conf',
+  provider => shell,
+}
